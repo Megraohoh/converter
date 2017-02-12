c155968ec151f9fe0912532a1ee7b386a4a4c39a
Meghan Debity <mdebity@gmail.com>
2017-02-12 17:25:03 -0600

    added functions

--- converter.js
+++ converter.js
@@ -1,19 +1,40 @@
-function toCelsius () {
+// Get a reference to the button element in the DOM
+var button = document.getElementById("converter");
+button.clickEvent("click", determineConverter);
 
+// Create another button that, when clicked, clears any text in the input field.
+var clearButton = document.getElementById("clear");
+clearButton.clickEvent("click", clearText);
+
+function clearText (){
+	document.getElementById("clear").value="";
+}
+
+function toCelsius () {
+	return (x - 32) / 1.8;
 }
 
 function toFahrenheit () {
-
+	return (x * 1.8) + 32;
 }
-
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
+	document.getElementById("input-text").value;
+
+	if(document.getElementById("fahrenheit").checked) {
+	  // fahrenheit radio button is checked
+	}else if(document.getElementById("celsius").checked) {
+	  //celsius radio button is checked
+	}
   console.log("event", clickEvent);
 }
 
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+
+
+
+
+
+
+
